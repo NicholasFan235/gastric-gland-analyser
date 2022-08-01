9f7a161ad960b26e8b5cfea72f216e546cc45706
Nicholas Fan <nicholas.lj.fan@gmail.com>
2022-08-01 13:49:19 +0100

    update

--- gastric_gland_analyser/analysers/helpers/_count_cells.py
+++ gastric_gland_analyser/analysers/helpers/_count_cells.py
@@ -3,7 +3,8 @@ import numpy as np
 import functools
 
 def _count_cells(population:Population, filter=None):
-    return sum(1 if filter(cell) else 0 for cell in population.cells)
+    return len(population.cells) if filter is None\
+        else sum(1 if filter(cell) else 0 for cell in population.cells)
 
 def _count_neck_cells(population:Population):
     return _count_cells(population, lambda c: c.proliferative_cell_type=='neck')
